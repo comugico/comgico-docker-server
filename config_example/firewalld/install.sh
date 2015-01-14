4a289dc4474913798d10498394b45235600ed0c6
Shuma Yoshioka <s64.stdio@gmail.com>
2015-01-14 19:29:49 +0900

    Update

--- config_example/firewalld/install.sh
+++ config_example/firewalld/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 CURRENT=$(cd $(dirname $0) && pwd)
-cp ./service.xml /etc/firewalld/services/my-docker-platform.xml
+#cp ./service.xml /etc/firewalld/services/my-docker-platform.xml
 cp ./service.xml /etc/firewalld/services/comugico-docker-server.xml
-firewall-cmd --add-service=my-docker-platform --zone=internal --permanent
+#firewall-cmd --add-service=my-docker-platform --zone=internal --permanent
 firewall-cmd --add-service=comugico-docker-server --zone=internal --permanent
 firewall-cmd --add-service=http --zone=public --permanent
 firewall-cmd --reload
