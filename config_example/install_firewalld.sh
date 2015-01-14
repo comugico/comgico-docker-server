f646ca902a0ee2788e030089146d07bafe34fbc7
Shuma Yoshioka <s64.stdio@gmail.com>
2015-01-14 22:15:26 +0900

    Update

--- config_example/install_firewalld.sh
+++ config_example/install_firewalld.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 CURRENT=$(cd $(dirname $0) && pwd)
 firewall-cmd --reload
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -p tcp --dport 4243 -j REJECT
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 1 -p tcp --dport 4243 -i docker0 -j ACCEPT
-firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 2 -p tcp --dport 4243 -s 172.17.42.0/24 -j ACCEPT
-firewall-cmd --reload
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -p tcp --dport 4243 -s 172.17.42.0/24 -j ACCEPT
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 1 -p tcp --dport 4243 -j REJECT
 firewall-cmd --permanent --add-service=http --zone=public
+firewall-cmd --reload
+service firewalld restart
+service docker restart
+firewall-cmd --direct --get-all-rules
 firewall-cmd --permanent --list-services --zone=public
-firewall-cmd --permanent --direct --get-all-passthroughs
