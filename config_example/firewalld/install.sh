de6aebae15b6e281d6c15c86e6add25b888f0950
Shuma Yoshioka <s64.stdio@gmail.com>
2015-01-14 19:39:42 +0900

    Update

--- config_example/firewalld/install.sh
+++ config_example/firewalld/install.sh
@@ -2,6 +2,6 @@
 CURRENT=$(cd $(dirname $0) && pwd)
 cp ./service.xml /etc/firewalld/services/my-docker-platform.xml
 firewall-cmd --reload
-firewall-cmd --add-service=my-docker-platform --zone=internal --permanent
+firewall-cmd --add-service=my-docker-platform --zone=work --permanent
 firewall-cmd --add-service=http --zone=public --permanent
-firewall-cmd --list-services --zone=internal --permanent
+firewall-cmd --list-services --zone=work --permanent
